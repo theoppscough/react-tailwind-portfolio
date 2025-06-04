29ddd5c6476393189625f5b10bcedda04b482ca5
Kevin Ochoa <108596878+theoppscough@users.noreply.github.com>
2025-06-04 11:15:35 -0400

    Resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -42,7 +42,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://github.com/theoppscough/react-tailwind-portfolio/blob/main/Resume.docx%20(5).pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
